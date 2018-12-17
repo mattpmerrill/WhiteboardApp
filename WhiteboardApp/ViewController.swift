cb322728415e372363f26477bd58c3de8f4f1576
mattpmerrill <mattpmerrill@gmail.com>
2018-12-16 19:50:11 -0700

    Initial Commit

---
+++ WhiteboardApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhiteboardApp
+//
+//  Created by Matt Merrill on 12/16/18.
+//  Copyright © 2018 Matt Merrill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
